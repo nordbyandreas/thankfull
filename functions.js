a09895fb6c06db58727d0425302cc9c5683f12ef
nordbyandreas <nordbyandreas@hotmail.com>
2017-08-30 16:09:27 +0200

    uploading project

---
+++ functions.js
@@ -0,0 +1,18 @@
+
+
+
+
+
+document.getElementById('yellow').onclick = function() {
+  document.getElementById('colors').setAttribute('href', 'css/yellow.css');
+};
+
+
+
+document.getElementById('red').onclick = function() {
+  document.getElementById('colors').setAttribute('href', 'css/red.css');
+};
+
+document.getElementById('orange').onclick = function() {
+  document.getElementById('colors').setAttribute('href', 'css/orange.css');
+};
